dc2985dbc1c818de201d8b0e9f730808dfc82bd2
sohye-lee <sohyelee@gmail.com>
2024-04-29 12:36:15 -0400

    text on the canvas

--- components/Html/Html.jsx
+++ components/Html/Html.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 export default function Html({ htmlContent }) {
   const tag = React.createElement(`div`, {
+    className: "html-div",
     dangerouslySetInnerHTML: { __html: htmlContent },
   });
   return tag;
