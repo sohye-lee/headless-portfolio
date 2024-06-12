5304767382355098ca68a59244f53deb7281681c
sohye <sohyek@white64.com>
2024-06-11 22:48:39 -0400

    ga4 implement with pure code

--- components/Page/Page.jsx
+++ components/Page/Page.jsx
@@ -121,6 +121,15 @@ export default function Page(props) {
         </div>
       </div>
        <GoogleAnalytics gaId="G-TD6DE47WN3" />
+  
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-TD6DE47WN3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments)}
+  gtag('js', new Date());
+
+  gtag('config', 'G-TD6DE47WN3');
+</script>
     </>
   );
 }
