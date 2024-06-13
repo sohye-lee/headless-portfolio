1ced668a0c82f2ac3d7faa6e9550078ba733f4b4
sohye <sohyek@white64.com>
2024-06-13 07:16:16 -0400

    fix script

--- pages/_app.js
+++ pages/_app.js
@@ -22,12 +22,16 @@ function MyApp({ Component, pageProps }) {
   gtag('js', new Date());
 
   gtag('config', 'G-TD6DE47WN3');
-  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  `,
+          }}
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-W8B79JFS');
-  `,
+})(window,document,'script','dataLayer','GTM-W8B79JFS');`,
           }}
         ></script>
       </Head>
@@ -38,7 +42,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           src="https://www.googletagmanager.com/ns.html?id=GTM-W8B79JFS"
           height="0"
           width="0"
-          style="display:none;visibility:hidden"
+          style={{ display: "none", visibility: "hidden" }}
         ></iframe>
       </noscript>
     </>
