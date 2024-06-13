ab380bd192482960cb0bb3c2debb6e54d75ab5b9
sohye <sohyek@white64.com>
2024-06-13 06:47:01 -0400

    head added

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import "@fortawesome/fontawesome-svg-core/styles.css";
 import Script from "next/script";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import { env } from "env";
+import Head from "next/head";
 config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }) {
