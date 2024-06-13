4fe8c102486bf8d46d6a334401d76705a419fe64
sohye <sohyek@white64.com>
2024-06-13 07:13:04 -0400

    tag manager code imbed

--- pages/_app.js
+++ pages/_app.js
@@ -21,12 +21,26 @@ function MyApp({ Component, pageProps }) {
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-TD6DE47WN3');`,
+  gtag('config', 'G-TD6DE47WN3');
+  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-W8B79JFS');
+  `,
           }}
         ></script>
       </Head>
       <Component {...pageProps} />
       <GoogleAnalytics gaId="G-TD6DE47WN3" />
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-W8B79JFS"
+          height="0"
+          width="0"
+          style="display:none;visibility:hidden"
+        ></iframe>
+      </noscript>
     </>
   );
 }
